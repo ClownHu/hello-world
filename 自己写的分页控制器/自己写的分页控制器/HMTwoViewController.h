9140e7b5f12c07c4f77d14d35b73a47440d7ec72
ClownHu <1214215316@qq.com>
2017-03-06 14:20:22 +0800

    新创建了一个控制器

---
+++ 自己写的分页控制器/自己写的分页控制器/HMTwoViewController.h
@@ -0,0 +1,13 @@
+//
+//  HMTwoViewController.h
+//  自己写的分页控制器
+//
+//  Created by 胡卓 on 2017/3/6.
+//  Copyright © 2017年 胡卓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMTwoViewController : UIViewController
+
+@end
---
+++ 自己写的分页控制器/自己写的分页控制器/HMTwoViewController.m
@@ -0,0 +1,37 @@
+//
+//  HMTwoViewController.m
+//  自己写的分页控制器
+//
+//  Created by 胡卓 on 2017/3/6.
+//  Copyright © 2017年 胡卓. All rights reserved.
+//
+
+#import "HMTwoViewController.h"
+
+@interface HMTwoViewController ()
+
+@end
+
+@implementation HMTwoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
